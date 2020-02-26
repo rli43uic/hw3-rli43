66d048c744c7b92c1702f7d83b953affa58a9e6e
Raymond Li <rli43@uic.edu>
2020-02-26 13:20:56 -0600

    try 3

--- hw3.c
+++ hw3.c
@@ -19,137 +19,29 @@ void sigtstpHandler( int sig) {
     execute();
 }
 
-execute(){
+execute() {
 	char line[500];
-	while(1){
- 		printf("CS361 > ");
-//--------------------------------------Line Parsing
-		fgets(line, 500, stdin);
-		line[strlen(line) - 1] = '\0';
-		int i = 0;
-		char** argsarray = malloc(1000 * sizeof(char*));
+	while (TRUE) {
+		printf("CS351 >"); 
+		fgets(line, 500, stdin); //take in input from line
+		line[strlen(line) - 1] = '\0'; // string termination character
 		char* word;
+		//char ** argv = malloc(1000 * sizeof(char*)); 
+		char* argsList[20];
+		int i = 0;
+   		for(i=0; i < 20; ++i)
+			argsList[i] = (char*)malloc(100*sizeof(char));
+			
+		i = 0;
+		word = strtok(line, " "); // parse line into word
+		while (word) {
+			argsList[i] = word;
+			++i;
+			word = strtok(NULL, " ");//iterate
+		}
+		argsList[i] = NULL;
+		
 
-		word = strtok(line, " ");
-		while(word) {
-			argsarray[i] = word;
-			i++;
-			word = strtok(NULL, " ");
-		}
-		argsarray[i] = NULL;
-//--------------------------------------Arg Check
-	char *IO;
-	char *IO_2[100];
-	char *IO_Multi[100];
-	int flag = 0;
-	int temp = 0;
-	int pid = 0;
-	int pid_2 = 0;
-	int status = 0;
-	int status_2 = 0;
-	char *fd = '\0';
-	i = 0;
-	
-	while(1){
-		if(argsarray[i] == '\0'){
-			break;
-		}
-		if(!strcmp(argsarray[0], "exit")){
-			exit(0);
-			return 0;
- 		}
-		if(!strcmp(argsarray[i], ";")){
-			flag = 1;
-			IO_Multi[0] = argsarray[i + 1];
-			if(argsarray[i+2] != '\0'){
-				if((!strcmp(argsarray[i+2], ">")) || (!strcmp(argsarray[i+2], "<"))){
-					IO_2[0] = argsarray[i+2];
-					IO_2[1] = argsarray[i+3];
-				}
-				else{
-					IO_Multi[0] = argsarray[i+1];
-					IO_Multi[1] = argsarray[i+2];
-				}
-			}
-			break;
-		}
-		if(!strcmp(argsarray[i], "<")){
-			IO = argsarray[i];
-			fd = argsarray[i + 1];
-			if(argsarray[i + 2] != '\0'){
-				if(!strcmp(argsarray[i + 2], ";")){
-					flag = 1;
-					IO_Multi[0] = argsarray[i + 3];
-					if(argsarray[i+4] != '\0'){
-						IO_2[0] = argsarray[i + 4];
-						IO_2[1] = argsarray[i + 5];
-					}
-				}
-			}
-			break;
-		}
-		if(!strcmp(argsarray[i], ">")){
-			IO = argsarray[i];
-			fd = argsarray[i + 1];
-			if(argsarray[i + 2] != '\0'){
-				if(!strcmp(argsarray[i + 2], ";")){
-					flag = 1;
-					IO_Multi[0] = argsarray[i + 3];
-					if(argsarray[i+4] != '\0'){
-						IO_2[0] = argsarray[i + 4];
-						IO_2[1] = argsarray[i + 5];
-					}
-				}
-			}
-			break;
-		}
-		i++;
-	}
-	pid = fork();
-	if(pid == 0){
-		if(IO != '\0'){
-			if(!strcmp(IO, "<")){
-				int fd1 = open(fd, O_RDONLY);
-				dup2(fd1, 0);
-				close(fd1);
-			}
-			if(!strcmp(IO, ">")){
-				int fd1 = open(fd, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR|S_IXUSR|S_IROTH);
-				dup2(fd1, 1);
-				close(fd1);
-			}
-		}
-		exit(execv(IO_Multi[0], IO_Multi));
-	}
-	else {
-		wait(&status);
-		if(flag = 1){
-			pid_2 = fork();
-			if(pid_2 == 0){
-				if(IO_2[0] != '\0'){
-					if(!strcmp(IO_2[0], "<")){
-						int fd1 = open(fd, O_RDONLY);
-						dup2(fd1,0);
-						close(fd1);
-					}
-					if(!strcmp(IO_2[0], ">")){
-						int fd1 = open(fd, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR|S_IXUSR|S_IROTH);
-						dup2(fd1, 1);
-						close(fd1);
-					}
-				}
-				exit(execv(IO_Multi[0], IO_Multi));
-			}
-			else{
-				wait(&status_2);
-			}
-		}
-		printf("pid:%i status:%i\n", pid, status,  WEXITSTATUS(status));
-		if(pid_2 != 0){
-			printf("pid:%i status:%i\n", pid_2, status_2,  WEXITSTATUS(status_2));
-		}
-	}
-	}
 }
 
 int main(int argc, char* argv[])
